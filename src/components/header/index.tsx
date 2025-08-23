be907b0da4f07e90de66887f3e544fdfe98d2459
Thiago Pessoa <tpessoa96@gmail.com>
2025-08-23 08:53:52 -0300

    Criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,42 @@
+"use client"
+
+import { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
+import Link from 'next/link'
 
 export function Header(){
+    const [top, setTop] = useState(true)
+
+    const scrollHandle = () => {
+        window.scrollY > 10 ? setTop(false) : setTop(true)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandle)
+    },[top])
+
     return(
-        <header>
-            <h2>Header</h2>
+        <header className={`${styles.header} ${top ? styles.fixed : styles.background}`}>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.contentLogo}>
+                        <Link href={"/"}>
+                            DevMotors
+                        </Link>
+                    </div>
+                    <nav className={styles.nav}>
+                        <Link href={'/'}>
+                            HOME
+                        </Link>
+                        <Link href={'/#servicos'}>
+                            SERVIÇOS
+                        </Link>
+                        <Link href={'/#contatos'}>
+                            CONTATOS
+                        </Link>
+                    </nav>
+                </div>
+            </div>
         </header>
     )
 }
