eb2fb425c8874f047f0c9b4959c4b7673f67a670
Thiago Pessoa <tpessoa96@gmail.com>
2025-08-25 18:47:16 -0300

    Criando submenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Submenu } from "@/components/home/submenu";
 
 export default function Home() {
   return (
     <div>
-        teste
+      <Submenu/>        
     </div>
   );
 }
