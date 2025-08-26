00a5e838fd77ccaff9544b119bb2f5498fbf1877
Thiago Pessoa <tpessoa96@gmail.com>
2025-08-26 19:16:28 -0300

    Terminando submenu

--- src/components/home/submenu/index.tsx
+++ src/components/home/submenu/index.tsx
@@ -1,19 +1,49 @@
+"use client"
+
 import Link from "next/link"
 import styles from "./styles.module.scss"
 import { X, Menu } from "lucide-react"
+import { useEffect, useState } from "react"
 
-export function Submenu(){
+export function Submenu() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 728) {
+                setIsOpen(false)
+            }
+        }
+
+        window.addEventListener("resize", handleResize)
+
+        return window.removeEventListener("resize", handleResize)
+    }, [])
+
+    function toggleMenu() {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <section className={styles.submenu}>
-            <div className={styles.submenuIcon}>
-                <Menu size={34} color="#121212"/>
+            <div className={styles.submenuIcon} onClick={toggleMenu}>
+                <Menu size={34} color="#121212" />
                 Menu
             </div>
-            <ul className={styles.ul}>
+            <ul className={`${styles.ul} ${isOpen ? styles.open : ''}`}>
+
+                {isOpen && (
+                    <button className={styles.closeButton} onClick={toggleMenu}>
+                        <X size={54} color="#121212"/>
+                    </button>
+                )}
+
                 <li>
                     <Link href={"/"}>
                         Página 1
                     </Link>
+                </li>
+                <li>
                     <Link href="/">
                         Página 1
                     </Link>
